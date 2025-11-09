32e6fb3a36204ce6fe097e2e617438e4a92a30a6
nitzano707 <nitzan.orot@gmail.com>
2025-11-09 18:00:31 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <h1>🎧 Tamleli Pro</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
